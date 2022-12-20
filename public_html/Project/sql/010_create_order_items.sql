09f55d6dd6d8b4465318665832a1c2427f8a62fb
Nikita <nikitashaji1@gmail.com>
2022-12-20 14:46:50 -0500

    added OrderItems and Orders sql tables

--- public_html/Project/sql/010_create_order_items.sql
+++ public_html/Project/sql/010_create_order_items.sql
@@ -1,11 +1,11 @@
-CREATE TABLE IF NOT EXISTS OrderItems(
+CREATE TABLE IF NOT EXISTS `OrderItems`(
     id int AUTO_INCREMENT PRIMARY KEY,
     order_id int,
     item_id int,
     quantity int,
     user_id int,
     cost int,
-    FOREIGN KEY (user_id) REFERENCES Users(id),
-    FOREIGN KEY (item_id) REFERENCES Products(id),
+    FOREIGN KEY (`user_id`) REFERENCES Users(`id`),
+    FOREIGN KEY (`item_id`) REFERENCES Products(`id`),
     check(quantity > 0)
 )
