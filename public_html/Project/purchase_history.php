9ff90dfd26e0625b0911d61d37578c4516e182f1
Nikita <nikitashaji1@gmail.com>
2022-12-22 16:00:44 -0500

    OrderHistory and detail page for admin// added pagination and filters too

--- public_html/Project/purchase_history.php
+++ public_html/Project/purchase_history.php
@@ -1,91 +1,139 @@
 <?php
-require(__DIR__ . "/../../partials/nav.php");
+//note we need to go up 1 more directory
+require(__DIR__ . "/../../../partials/nav.php");
 
 is_logged_in(true);
-$query = "SELECT order.order_id, order.item_id, order.quantity, order.cost
-FROM Orders as item JOIN OrderItems as order on order.id = order.order_id
- WHERE order.user_id = :uid";
-$db = getDB();
-$stmt = $db->prepare($query);
-$cart = [];
+$db =getDB();
+$result = [];
+
+$col = se($_GET, "col", "created", false);
+if(!in_array($col, ["total_price", "created", "payment"])) {
+    $col = "created";
+}
+
+$order = se($_GET, "order", "asc", false);
+if(!in_array($order, ["asc", "desc"])) {
+    $order = "asc";
+}
+$name =se($_GET, "name", "", false);
+$name2 = se($_GET, "name2", "", false);
+
+$base_query = "SELECT id, address, total_price, payment, created FROM Orders";
+$total_query = "SELECT count(1) as total FROM Orders";
+
+$query = "WHERE 1=1";
+$params = [];
+
+if(!empty($name)) {
+    $query .= "AND created >= :d1 AND created <=:d2";
+    $params[":d1"] = "$name";
+    $params[":d2"] = "$name2 23:59:59";
+
+}
+
+if(!empty($col) && !empty($order)) {
+$query .= "ORDER BY $col $order";}
+
+$per_page = 10;
+paginate($total_query . $query, $params, $per_page);
+
+$query .= " LIMIT :offset, :count";
+$params[":offset"] = $offset;
+$params[":count"] = $per_page;
+
+$stmt = $db->prepare($base_query . $query);
+foreach ($params as $key => $value) {
+    $type = is_int($value) ? PDO::PARAM_INT : PDO::PARAM_STR;
+    $stmt->bindValue($key, $value, $type);
+}
+$params = null;
+
 try {
-    $stmt->execute([":uid" => get_user_id()]);
-    $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
-    if ($results) {
-        $cart = $results;
-        
+    $stmt->execute($params);
+    $r =$stmt->fetchAll(PDO::FETCH_ASSOC);
+    if ($r) {
+        $result = $r;
     }
 } catch (PDOException $e) {
-    error_log(var_export($e, true));
-    flash("Error fetching order history", "danger");
+    flash("<pre>" . var_export($e, true) . "</pre");
 }
 ?>
 
-<!DOCTYPE html>
-<html>
-<head>
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-<style>
-body{
-    background-color: rgb(199, 188, 222);
+<form class = "row row-cols-auto g-3 align-items-center">
+    <div class="col">
+        <div class="input-group">
+           <div class ="input-group-text">Date Start</div>
+           <input class="form-control" name="name" value="<?php se($name); ?>"/>
+        </div>
+    </div>
+    <div class="col">
+        <div class="input-group">
+           <div class ="input-group-text">Date End</div>
+           <input class="form-control" name="name2" value="<?php se($name2); ?>"/>
+        </div>
+    </div>
+    <div class="col">
+        <div class="input-group">
+            <div class="input-group-text">Sort</div>
+            <select class="form-control" name="col" value="<?php se($col); ?>">
+            <option value="total_price">Total</option>
+            <option value="created">Date Purchased</option>
+            <option value="total_price">Payment Type</option>
+        </select>
+        <script>
+            document.forms[0].col.value = "<?php se($col); ?>";
+            </script>
+            <select class="form-control" name="order" value="<?php se($order); ?>">
+            <option value="asc">Up</option>
+            <option value="desc">Down</option>
+            </select>
+        <script>
+            document.forms[0].order.value = "<?php se($order); ?>";
+        </script> 
+
+        </div>
+    </div>
+        <div class="col">
+            <div class="input-group">
+            <input type="submit" class="btn btn-primary" value="Apply"/>
+            </div>
+        </div>
     
-
-}
-
-</style>
-</head>
+</form>
 
 
-<div class="container-fluid">
+<?php $totalValue = 0;?>
+<div class ="container-fluid">
     <h1>Order History</h1>
-    <table class="table table-striped">
-        <?php $total = 0; ?>
+    <table class="table text-dark">
         <thead>
-            <tr>
-                <th>Item</th>
-                <th>Price</th>
-                <th>Quantity</th>
-                <th>Subtotal</th>
-                
-            </tr>
-        </thead>
-        <tbody>
-        <?php foreach ($cart as $c) : ?>
-            <tr>
-                <td><?php se($c, "name"); ?></td>
-                <td><?php se($c, "unit_price"); ?></td>
+            <th>Total Price</th>
+            <th>Payment Visa</th>
+            <th>Order Date</th>
+            <th>Address</th>
+            <th>More Details</th>
 
+        </thead>
+        <?php foreach ($result as $item) : ?>
+            <tbody>
+                <td><?php se($item, "total_price"); ?></td>
+                <td><?php se($item, "payment"); ?></td>
+                <td><?php se($item, "created"); ?></td>
+                <td><?php se($item, "address"); ?></td>
+                <?php $totalValue = $totalValue + intval(se($item, "total_price", "", false));?>
                 <td>
-                <?php se($c, "desired_quantity"); ?>
-                </td>
-                <?php $total += (int)se($c, "subtotal", 0, false); ?>
-                <td><?php se($c, "subtotal"); ?></td>
-                <td>
-                    <form method="POST">
-                        <input type="hidden" name="cart_id" value="<?php se($c, "id"); ?>" />
-                        <input type="hidden" name="action" value="delete" />
-                         
-                        <a href="order_info.php?item_id=<?php se($item, "id"); ?>">Details</a>
+                    <form method ="POST" action="order_info.php">
+                        <input class="btn btn-primary" type="submit" value="More Info" name="OrderInfo"/>
+                        <input type= "hidden" name="order_id" value="<?php se($item, "id"); ?>"/>
                     </form>
                 </td>
-            </tr>
-        <?php endforeach; ?>
-        <?php if (count($cart) == 0) : ?>
-            <tr>
-                <td colspan="100%">No items in cart</td>
-            </tr>
-        <?php endif; ?>
-        
-        
-        <tr>
-            <td colspan="100%">Total: $<?php se($total, null, 0); ?></td>
-        </tr>
-        
-        </tbody>
-        
+            </tbody>
+            <?php endforeach; ?>
+            <h2> Total Revenue for All Orders: $<?php echo($totalValue);?></h2>
+            
     </table>
-    </div>
-<?php
-require(__DIR__ . "/../../partials/flash.php");
-?>
+    
+
+</div>    
+        
+<?php require(__DIR__ . "/../../partials/flash.php");?>
