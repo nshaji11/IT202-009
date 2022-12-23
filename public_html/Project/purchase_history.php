2e3c634f670cde13e297cf0813ce558c0e769d0b
Nikita <nikitashaji1@gmail.com>
2022-12-22 20:07:43 -0500

    fixed purchase history

--- public_html/Project/purchase_history.php
+++ public_html/Project/purchase_history.php
@@ -1,6 +1,6 @@
 <?php
 //note we need to go up 1 more directory
-require(__DIR__ . "/../../../partials/nav.php");
+require(__DIR__ . "/../../partials/nav.php");
 
 is_logged_in(true);
 $db =getDB();
